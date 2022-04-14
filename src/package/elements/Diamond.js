55d1141da91cf370bd226f15b2d7b03c5927a393
wanglin25 <wanglin25@hikvision.com.cn>
2022-04-14 19:50:20 +0800

    新增菱形和三角形绘制

--- src/package/elements/Diamond.js
+++ src/package/elements/Diamond.js
@@ -1,17 +1,15 @@
 import BaseElement from "./BaseElement";
-import { drawCircle } from "../utils/draw";
+import { drawDiamond } from "../utils/draw";
 import DragElement from "./DragElement";
 import { transformPointOnElement } from "../utils";
-import { getCircleRadius, checkIsAtCircleEdge } from "../utils/checkHit";
+import { checkIsAtDiamondEdge } from "../utils/checkHit";
 
-// 正圆元素类
-export default class Circle extends BaseElement {
+// 菱形元素类
+export default class Diamond extends BaseElement {
   constructor(...args) {
     super(...args);
     // 拖拽元素实例
-    this.dragElement = new DragElement(this, this.app, {
-        lockRatio: true
-    });
+    this.dragElement = new DragElement(this, this.app);
   }
 
   // 渲染到画布
@@ -19,7 +17,7 @@ export default class Circle extends BaseElement {
     let { width, height } = this;
     this.warpRender(({ halfWidth, halfHeight }) => {
       // 画布中心点修改了，所以元素的坐标也要相应修改
-      drawCircle(this.ctx, 0, 0, getCircleRadius(width, height), true);
+      drawDiamond(this.ctx, -halfWidth, -halfHeight, width, height, true);
     });
     // 激活时显示拖拽框
     if (this.isActive && !this.isCreating) {
@@ -30,6 +28,6 @@ export default class Circle extends BaseElement {
   // 检测是否被击中
   isHit(x, y) {
     let rp = transformPointOnElement(x, y, this);
-    return checkIsAtCircleEdge(this, rp);
+    return checkIsAtDiamondEdge(this, rp);
   }
 }
